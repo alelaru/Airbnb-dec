e731e9f034335a5ee4e71d9f60378dfe111b8229
Alelaru <alelaru@gmail.com>
2022-06-17 20:18:47 +0200

    Airbnb homepage bar ready with web3uiKit elements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import  bg from "../images/frontpagebg.png"
 import  logo from "../images/airbnb.png"
-import { ConnectButton } from "web3uikit";
+import { ConnectButton, DatePicker, Input, Select } from "web3uikit";
 
 
 const Home = () => {
@@ -26,6 +26,68 @@ const Home = () => {
           <ConnectButton></ConnectButton>
         </div>
       </div>
+
+      <div className="tabContent">
+        <div className="searchFields">
+          <div className="inputs">
+            Location
+            <Select
+              defaultOptionIndex={0}
+              onChange={(data) => console.log(data.label)}
+              options={[
+                {
+                  id: "ny",
+                  label: "New York"
+                },
+                {
+                  id: "lon",
+                  label: "London"
+                },
+                {
+                  id: "db",
+                  label: "Dubai"
+                },                                
+                {
+                  id: "la",
+                  label: "Los Angeles"
+                },
+                {
+                  id: "mc",
+                  label: "Mexico City"
+                },
+              ]}
+            ></Select>
+          </div>
+          <div className="vl"></div>
+          <div className="inputs">
+            Check-in
+            <DatePicker
+              id="check-in"
+              onChange={(e) => console.log(e.data)}
+            ></DatePicker>
+          </div>
+          <div className="vl"></div>
+          <div className="inputs">
+            Check-out
+            <DatePicker
+              id="check-out"
+              onChange={(e) => console.log(e.data)}
+            ></DatePicker>  
+          </div>
+          <div className="vl"></div>
+          <div className="inputs">
+            Guests
+            <Input
+              value={2}
+              name="AddGuests"
+              type="number"
+              onChange={e => console.log(e.data) }
+            >
+            </Input>
+          </div>
+        </div>
+      </div>
+
     </>
   );
 };
